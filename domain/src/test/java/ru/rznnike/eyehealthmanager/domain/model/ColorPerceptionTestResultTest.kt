6e50bfb4724d89c7f46c18ac4515b61ad6419f74
RznNike <rznnike@yandex.ru>
2024-01-01 05:24:24 +0300

    ColorPerceptionTestResult tested

---
+++ domain/src/test/java/ru/rznnike/eyehealthmanager/domain/model/ColorPerceptionTestResultTest.kt
@@ -0,0 +1,58 @@
+package ru.rznnike.eyehealthmanager.domain.model
+
+import org.junit.jupiter.api.Assertions.*
+
+import org.junit.jupiter.api.Test
+import ru.rznnike.eyehealthmanager.domain.model.enums.AstigmatismAnswerType
+
+class ColorPerceptionTestResultTest {
+    private val testResult1 = ColorPerceptionTestResult(
+        id = 1,
+        timestamp = 1704029085000,
+        recognizedColorsCount = 35,
+        allColorsCount = 39
+    )
+
+    @Test
+    fun exportToString_data_success() {
+        val expectedResult = "31.12.2023 16:24:45\t35\t39"
+
+        val exportString = testResult1.exportToString()
+
+        assertEquals(expectedResult, exportString)
+    }
+
+    @Test
+    fun importFromString_correctData_success() {
+        val string = "31.12.2023 16:24:45\t35\t39"
+
+        val testResult = ColorPerceptionTestResult.importFromString(string)
+
+        assert(testResult1.contentEquals(testResult))
+    }
+
+    @Test
+    fun importFromString_corruptedData_null() {
+        val string = "qwerty"
+
+        val testResult = ColorPerceptionTestResult.importFromString(string)
+
+        assertNull(testResult)
+    }
+
+    @Test
+    fun contentEquals_sameData_true() {
+        val otherTest = ColorPerceptionTestResult.importFromString(testResult1.exportToString())
+
+        assert(testResult1.contentEquals(otherTest))
+    }
+
+    @Test
+    fun contentEquals_differentData_false() {
+        val string = "31.12.2023 16:24:45\t36\t39"
+
+        val otherTest = ColorPerceptionTestResult.importFromString(string)
+
+        assert(!testResult1.contentEquals(otherTest))
+    }
+}
