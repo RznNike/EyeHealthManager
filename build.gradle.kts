f8c9828f757bc45e4b9451662b6402be58de298b
RznNike <rznnike@yandex.ru>
2024-01-03 23:02:06 +0300

    Libs updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ buildscript {
         set("MIN_SDK", 24)
         set("TARGET_SDK", 34)
 
-        set("kotlinVersion", "1.9.0")
+        set("kotlinVersion", "1.9.22")
         set("coroutinesVersion", "1.7.3")
         set("objectboxVersion", "3.7.1")
         set("koinVersion", "3.5.3")
@@ -18,7 +18,7 @@ buildscript {
         maven(url = "https://jitpack.io")
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("com.android.tools.build:gradle:8.2.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${rootProject.extra["kotlinVersion"]}")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
         classpath("com.google.gms:google-services:4.4.0")
