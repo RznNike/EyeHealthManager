30de83ebbef125bad156922b3750f313b0e5a716
RznNike <rznnike@yandex.ru>
2024-01-06 01:33:22 +0300

    SplashPresenter tested

---
+++ app/src/test/java/ru/rznnike/eyehealthmanager/app/presentation/splash/SplashPresenterTest.kt
@@ -0,0 +1,26 @@
+package ru.rznnike.eyehealthmanager.app.presentation.splash
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mock
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
+import org.mockito.junit.jupiter.MockitoExtension
+import ru.rznnike.eyehealthmanager.app.Screens
+import ru.rznnike.eyehealthmanager.app.utils.screenMatcher
+
+@ExtendWith(MockitoExtension::class)
+class SplashPresenterTest {
+    @Mock
+    private lateinit var mockView: SplashView
+
+    @Test
+    fun onAnimationEnd_replaceWithMainFlow() {
+        val presenter = SplashPresenter()
+        presenter.attachView(mockView)
+
+        presenter.onAnimationEnd()
+
+        verify(mockView, times(1)).routerNewRootFlow(screenMatcher(Screens.Flow.main()))
+    }
+}
--- app/src/test/java/ru/rznnike/eyehealthmanager/app/utils/TestUtils.kt
+++ app/src/test/java/ru/rznnike/eyehealthmanager/app/utils/TestUtils.kt
@@ -1,6 +1,8 @@
 package ru.rznnike.eyehealthmanager.app.utils
 
+import com.github.terrakok.cicerone.androidx.FragmentScreen
 import kotlinx.coroutines.CoroutineScope
+import org.mockito.kotlin.argForWhich
 import ru.rznnike.eyehealthmanager.domain.global.CoroutineProvider
 
 fun CoroutineScope.createTestCoroutineProvider() = object : CoroutineProvider {
@@ -8,4 +10,8 @@ fun CoroutineScope.createTestCoroutineProvider() = object : CoroutineProvider {
     override val scopeMain = this@createTestCoroutineProvider
     override val scopeMainImmediate = this@createTestCoroutineProvider
     override val scopeUnconfined = this@createTestCoroutineProvider
+}
+
+fun screenMatcher(sampleScreen: FragmentScreen) = argForWhich<FragmentScreen> {
+    (screenKey == sampleScreen.screenKey)
 }
